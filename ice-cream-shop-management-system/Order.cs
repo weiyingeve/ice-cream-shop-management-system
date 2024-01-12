0598f034eefed50ebdbe15761348c71315172cc8
weiyingeve <smarteverest11@gmail.com>
2024-01-12 18:34:15 +0800

    edited order.cs ModifyIceCream() method

--- ice-cream-shop-management-system/Order.cs
+++ ice-cream-shop-management-system/Order.cs
@@ -24,7 +24,43 @@ namespace ice_cream_shop_management_system
         }
         public void ModifyIceCream(int orderid)
         {
-            throw new Exception(); //edit later
+            int orderno = orderid - 1;
+            Console.WriteLine("Enter new option: ");
+            IceCreamList[orderno].Option = Console.ReadLine();
+            Console.WriteLine("Enter new number of scoops: ");
+            IceCreamList[orderno].Scoops = Convert.ToInt32(Console.ReadLine());
+            IceCreamList[orderno].Flavours.Clear();
+            Console.WriteLine("Enter new flavour type (or nil to stop adding): ");
+            string flavourtype = Console.ReadLine();
+            Console.WriteLine("Is it premium? (True/False): ");
+            bool flavourpremium = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Enter new flavour quantity: ");
+            int flavourquantity = Convert.ToInt32(Console.ReadLine());
+            Flavour flavour = new Flavour(flavourtype, flavourpremium, flavourquantity);
+            IceCreamList[orderno].Flavours.Add(flavour);
+            while (flavourtype != "nil")
+            {
+                Console.WriteLine("Enter new flavour (or nil to stop adding): ");
+                flavourtype = Console.ReadLine();
+                Console.WriteLine("Is it premium? (True/False): ");
+                flavourpremium = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine("Enter new flavour quantity: ");
+                flavourquantity = Convert.ToInt32(Console.ReadLine());
+                flavour = new Flavour(flavourtype, flavourpremium, flavourquantity);
+                IceCreamList[orderno].Flavours.Add(flavour);
+            }
+            IceCreamList[orderno].Toppings.Clear();
+            Console.WriteLine("Enter new topping (or nil to stop adding): ");
+            string toppingtype = Console.ReadLine();
+            Topping topping = new Topping(toppingtype);
+            IceCreamList[orderno].Toppings.Add(topping);
+            while (toppingtype != "nil")
+            {
+                Console.WriteLine("Enter new topping (or nil to stop adding): ");
+                toppingtype = Console.ReadLine();
+                topping = new Topping(toppingtype);
+                IceCreamList[orderno].Toppings.Add(topping);
+            }
         }
         public void AddIceCream(IceCream iceCream)
         {
